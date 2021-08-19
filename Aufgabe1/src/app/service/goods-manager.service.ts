8623c4a3ec5bd6e9989d1f331b27c7f98453eebe
Bernardo Barcia <bbarciacordero@gmail.com>
2021-08-19 23:46:41 +0200

    Goods Manager Service

--- Aufgabe1/src/app/service/goods-manager.service.ts
+++ Aufgabe1/src/app/service/goods-manager.service.ts
@@ -5,9 +5,41 @@ import { Good } from '../model/good';
   providedIn: 'root',
 })
 export class GoodsManagerService {
-  private goods: Good[] = [];
-  private salesTax: number | undefined;
-  private totalTax: number | undefined;
+  private goods: Good[];
+  // Calculated Values
+  private salesTax: number = 0;
+  private totalPrice: number = 0;
 
-  constructor() {}
+  constructor(goodsList: Good[] = []) {
+    this.goods = goodsList;
+  }
+
+  getTotalTax() {
+    return this.salesTax;
+  }
+
+  getTotalPrice() {
+    return this.totalPrice;
+  }
+
+  calculateTotalTax() {
+    this.salesTax = 0;
+    this.goods.forEach((good) => {
+      this.salesTax += good.getSalesTaxCalculated();
+    });
+    return this.salesTax;
+  }
+
+  calculateTotalPrice() {
+    this.totalPrice = 0;
+    this.goods.forEach((good) => {
+      this.totalPrice += good.getTotalPrice();
+    });
+    return this.totalPrice;
+  }
+
+  calculateReceipt() {
+    this.calculateTotalTax();
+    this.calculateTotalPrice();
+  }
 }
