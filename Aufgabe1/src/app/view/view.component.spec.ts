573ff8a33e20a9a22560500145fcf0cdd6a76615
Bernardo Barcia <bbarciacordero@gmail.com>
2021-08-23 15:07:47 +0200

    View Testing

--- Aufgabe1/src/app/view/view.component.spec.ts
+++ Aufgabe1/src/app/view/view.component.spec.ts
@@ -24,6 +24,43 @@ describe('ViewComponent', () => {
 
   it('starting values', () => {
     expect(component.goodList).toBeTruthy();
-    expect(component.goodList.length).toBe(0);
+    expect(component.goodList.length).toBe(10);
+  });
+
+  it('starting UI', () => {
+    const native = fixture.nativeElement;
+    expect(
+      native
+        .querySelector('.calculatedReceipt')
+        ?.classList.contains('invisible')
+    ).toBeTrue();
+    expect(native.querySelector('.receipt-button')).toBeTruthy();
+    expect(native.querySelector('.goods-list')).toBeTruthy();
+  });
+
+  it('cannot generate empty receipt', () => {
+    component.generateReceipt();
+    expect(
+      fixture.nativeElement
+        .querySelector('.calculatedReceipt')
+        ?.classList.contains('invisible')
+    ).toBeTrue();
+  });
+
+  it('can change product amount and generate receipt', () => {
+    component.changeAmount(component.goodList[0], -1);
+    expect(component.goodList[0].amount).toBe(0);
+
+    component.changeAmount(component.goodList[0], 1);
+    expect(component.goodList[0].amount).toBe(1);
+
+    component.generateReceipt();
+    const receipt = fixture.nativeElement.querySelector('.receipt');
+    expect(
+      fixture.nativeElement
+        .querySelector('.calculatedReceipt')
+        ?.classList.contains('invisible')
+    ).toBeFalse();
+    expect(receipt.innerHTML.length).toBeGreaterThan(0);
   });
 });
